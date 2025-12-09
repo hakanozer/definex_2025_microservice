4d6beba0461b22b3cd73b50495dc60fdff33f8c5
Hakan ÖZER <hakanozer02@gmail.com>
2025-12-09 16:29:19 +0300

    Config Server

--- services/Note/src/main/java/com/works/note/NoteRestController.java
+++ services/Note/src/main/java/com/works/note/NoteRestController.java
@@ -1,5 +1,6 @@
 package com.works.note;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,16 @@ import java.util.Map;
 @RequestMapping("note")
 public class NoteRestController {
 
+    @Value("${definex.data}")
+    private String definexData;
+
     @GetMapping("all")
     public Map all(){
         Map<String, Object> mp = new LinkedHashMap<>();
         mp.put("status", false);
         mp.put("message", "Note List");
         mp.put("count", 50);
+        mp.put("data", definexData);
         return mp;
     }
 
